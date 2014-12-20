78b19726976d310ec9d842eef90355cef2409227
sibabl <sibabl@yahoo.com>
2014-12-20 14:38:45 -0500

    Revert "container items system"
    This reverts commit 6a5236651d02e1a05de31114d4d6bd6b86572ba2.

--- src/gui/container/items/items.cs
+++ src/gui/container/items/items.cs
@@ -1,69 +1,66 @@
 if(!isObject("ContainerItems"))
 new ScriptGroup(ContainerItems){};
 
-function ContainerItems::createData(%this, %category, %name, %image)
+function ContainerItems::createData(%this, %name, %image)
 {
 	if(isObject(%data = ("Inv" @ %name @ "Data")))
-	%data.delete();
+	return;
 
-	%path = "./" @ %category @ "/" (%image $= "" ? %name : %image ) @ ".png";
+	%path = "Add-Ons/Client_CityMod/res/gui/Items/";
+	%image = %path @ (%image $= "" ? %name : %image);
 
-	if(!isFile(%path))
-	{
-		error("ContainerItems.createData: invalid category path or item not found.");
-		return;
-	}
-
-	%itemData = new ScriptObject(%data)
+	%item = new ScriptObject(%data)
 	{
 		uiName = %name;
-		image = %path;
+		iconName = (isFile(%image @ ".png") ? %image : %path @ "none");
 	};
-	%this.add(%itemData);
+	%this.add(%item);
 
 	return %item;
 }
 
-ContainerItems.createData("Food", "Apple");
-ContainerItems.createData("Food", "Bacon");
-ContainerItems.createData("Food", "Baguette");
-ContainerItems.createData("Food", "Banana");
-ContainerItems.createData("Food", "Bananas", "BananaBundle");
-ContainerItems.createData("Food", "Pepper", "Bellpepper");
-ContainerItems.createData("Food", "Berries");
-ContainerItems.createData("Food", "Bread");
-ContainerItems.createData("Food", "Carrot");
-ContainerItems.createData("Food", "Carrots", "CarrotBundle");
-ContainerItems.createData("Food", "Corn");
-ContainerItems.createData("Food", "Drumbell");
-ContainerItems.createData("Food", "Lemon");
-ContainerItems.createData("Food", "Onion");
-ContainerItems.createData("Food", "Orange");
-ContainerItems.createData("Food", "Pear");
-ContainerItems.createData("Food", "Potatos", "PotatoBundle");
-ContainerItems.createData("Food", "Ribs");
-ContainerItems.createData("Food", "Steak");
-ContainerItems.createData("Food", "Tomato");
+function ContainerItems::createItem(%this, %data, %array)
+{
+	if(!isObject(%data))
+	return echo("\c2ERROR: ContainerItems datablock not found" SPC %data);
 
-// function ContainerItems::createItem(%this, %data, %array)
-// {
-// 	if(!isObject(%data))
-// 	return echo("\c2ERROR: ContainerItems datablock not found" SPC %data);
+	%item = new ScriptObject()
+	{
+		uiName = %data.uiName;
+		iconName = %data.iconName;
+	};
+	%this.add(%item);
 
-// 	%item = new ScriptObject()
-// 	{
-// 		uiName = %data.uiName;
-// 		iconName = %data.iconName;
-// 	};
-// 	%this.add(%item);
+	for(%i=0;%i<getFieldCount(%array);%i+=2)
+	{
+		%type = getField(%array, %i);
+		%val = getField(%array, %i+1);
 
-// 	for(%i=0;%i<getFieldCount(%array);%i+=2)
-// 	{
-// 		%type = getField(%array, %i);
-// 		%val = getField(%array, %i+1);
+		eval("%item." @ %type @ "=%val;");
+	}
 
-// 		eval("%item." @ %type @ "=%val;");
-// 	}
+	return %item;
+}
 
-// 	return %item;
-// }
+ContainerItems.createData("Apple");
+ContainerItems.createData("Bacon");
+ContainerItems.createData("Baguette");
+ContainerItems.createData("Banana");
+ContainerItems.createData("Bananas", "BananaBundle");
+ContainerItems.createData("Pepper", "Bellpepper");
+ContainerItems.createData("Berries");
+ContainerItems.createData("Bread");
+ContainerItems.createData("Carrot");
+ContainerItems.createData("Carrots", "CarrotBundle");
+ContainerItems.createData("Corn");
+ContainerItems.createData("Drumbell");
+ContainerItems.createData("Lemon");
+ContainerItems.createData("Onion");
+ContainerItems.createData("Orange");
+ContainerItems.createData("Pear");
+ContainerItems.createData("Potatos", "PotatoBundle");
+ContainerItems.createData("Ribs");
+ContainerItems.createData("Steak");
+ContainerItems.createData("Tomato");
+
+ContainerItems.createData("TestHat");
