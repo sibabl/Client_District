5b9d470b057783dc881067e62bfec364574caaf4
Arunatari <xalosdocs@gmail.com>
2014-12-21 00:18:15 -0500

    Added version control code to client.cs

--- client.cs
+++ client.cs
@@ -1,2 +1,21 @@
+$District::Version = 0;
+
+function District_HandshakePing(%value)
+{
+	commandToServer('District_HandshakePong', $District::Version);
+	$District::ServerVersion = %value;
+	$District::Connected = 1;
+}
+
+package DistrictClient
+{
+	function disconnect()
+	{
+		Parent::disconnect();
+		$District::Connected = 0;
+	}
+};
+activatePackage("DistrictClient");
+
 exec("./lib/init.cs");
 exec("./src/init.cs");
